d7823ec233ce6adbb8dcdd1856b5623fbe6ad7e4
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-06-20 12:22:18 +0200

    feat: create Components, use Props to send data trought components and state management to update component state, use conditional content and dynamic list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,71 @@
+import { useState } from "react";
+
+import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
+
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
+
 function App() {
+  let [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              onClick={() => handleClick("components")}
+              isSelected={selectedTopic == "components"}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              onClick={() => handleClick("jsx")}
+              isSelected={selectedTopic == "jsx"}
+            >
+              Jsx
+            </TabButton>
+            <TabButton
+              onClick={() => handleClick("props")}
+              isSelected={selectedTopic == "props"}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              onClick={() => handleClick("state")}
+              isSelected={selectedTopic == "state"}
+            >
+              State
+            </TabButton>
+          </menu>
+
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : (
+            <p>Please select a topic.</p>
+          )}
+        </section>
       </main>
     </div>
   );
